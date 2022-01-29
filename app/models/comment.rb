3250ef15e91b55d84686b99c31e97453b0ec3211
victor-chukwu <okenwa1993@gmail.com>
2022-01-29 17:35:00 +0100

    Fixing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
-  
+
   after_save :increase_comments_counter
 
   def increase_comments_counter
